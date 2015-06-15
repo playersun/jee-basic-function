9b8b65012b29c4d72dd1990daaee926be99cc574
pleyersun <sunhui1101@gmail.com>
2015-06-15 15:49:45 +0800

    将基本类型改为包装类型；参考spring data jpa中CrudRepositor新增CrudDao

---
+++ src/main/java/com/playersun/jbf/common/persistence/BaseDao.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2014-2016 https://github.com/playersun
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.playersun.jbf.common.persistence;
+
+import java.io.Serializable;
+
+/**
+ * @author PlayerSun
+ * @date Jun 10, 2015
+ */
+public interface BaseDao<T> {
+    
+}
---
+++ src/main/java/com/playersun/jbf/common/persistence/CrudDao.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2014-2016 https://github.com/playersun
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.playersun.jbf.common.persistence;
+
+/**
+ * <p>
+ * 抽象DAO层基类 提供一些简便方法<br />
+ * 参考spring data jpa中CrudRepository{@see
+ * org.springframework.data.repository.CrudRepository}
+ * </p>
+ * 
+ * @author PlayerSun
+ * @date Jun 10, 2015
+ */
+public interface CrudDao<T> extends BaseDao<T> {
+    
+    /**
+     * 根据id获得实体
+     * 
+     * @param id
+     * @return
+     */
+    public T findById(Long id);
+    
+    /**
+     * 保存实体到数据库
+     * 
+     * @param entity
+     */
+    public int insert(T entity);
+    
+    /**
+     * 更新实体到数据库
+     * 
+     * @param entity
+     * @return
+     */
+    public int update(T entity);
+    
+    /**
+     * 根据ID删除数据库中实体
+     * 
+     * @param id
+     * @return
+     */
+    public int delete(Long id);
+    
+    /**
+     * 删除数据库中实体
+     * 
+     * @param entity
+     * @return
+     */
+    public int delete(T entity);
+    
+    /**
+     * 删除所有
+     * 
+     * @return
+     */
+    public int delteAll();
+}

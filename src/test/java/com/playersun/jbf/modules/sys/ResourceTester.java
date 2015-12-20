7b0c5d6ffbbd90ebb2e2656fa7079aa337a4e7da
pleyersun <sunhui1101@gmail.com>
2015-12-20 11:21:06 +0800

    修复编译错误

--- src/test/java/com/playersun/jbf/modules/sys/ResourceTester.java
+++ src/test/java/com/playersun/jbf/modules/sys/ResourceTester.java
@@ -5,23 +5,15 @@
  */
 package com.playersun.jbf.modules.sys;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.google.common.collect.Sets;
 import com.playersun.jbf.common.BaseTest;
-import com.playersun.jbf.common.persistence.mybatis.pagination.PageMybatis;
-import com.playersun.jbf.common.persistence.mybatis.pagination.PageRequest;
-import com.playersun.jbf.common.persistence.pagination.Pageable;
-import com.playersun.jbf.common.persistence.pagination.SortDirection;
-import com.playersun.jbf.common.persistence.pagination.SortField;
 import com.playersun.jbf.modules.sys.entity.Resource;
 import com.playersun.jbf.modules.sys.service.ResourceService;
 
@@ -109,11 +101,11 @@ public class ResourceTester extends BaseTest {
         Resource r = new Resource();
         r.setParentId(1L);
         
-        List<SortField> list = new ArrayList<SortField>();
+        /*List<SortField> list = new ArrayList<SortField>();
         list.add(new SortField("id", SortDirection.DESC));
         
         Pageable pageable = new PageRequest(6, 3, map, list);
         PageMybatis<Resource> p = resourceService.findList(pageable);
-        System.out.print(p);
+        System.out.print(p);*/
     }
 }
